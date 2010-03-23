ca80be624c14e70a466e80826ca710448253b4be
Anders Pearson <anders@columbia.edu>
2010-03-23 14:40:04 -0400

    update gear view added

--- apps/gear/urls.py
+++ apps/gear/urls.py
@@ -11,10 +11,12 @@ urlpatterns = patterns('',
                        (r'^$', 'django.views.generic.list_detail.object_list', info_dict),
                        (r'^create/?$', 'django.views.generic.create_update.create_object',
                         dict(model=Gear, post_save_redirect="/gear/") ),
+
                        url(r'^tag/(?P<tag>[^/]+)/$',tagged_object_list,dict(queryset_or_model=Gear, paginate_by=100, allow_empty=True,
                                                                             template_name="gear/gear_tag_list.html"),
                            name='gear_tag_detail'),
-
+                       (r'^(?P<object_id>\d+)/update/?$', 'django.views.generic.create_update.update_object',
+                        dict(model=Gear, post_save_redirect="/gear/") ),
                        (r'^(?P<object_id>\d+)/$', 'django.views.generic.list_detail.object_detail', info_dict),
 
 )
