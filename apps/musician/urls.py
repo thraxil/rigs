2b116f702ed4d774de00d221d4ae91f38e5fd5db
Anders Pearson <anders@columbia.edu>
2010-03-30 16:01:53 -0400

    musician tag view

--- apps/musician/urls.py
+++ apps/musician/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import *
 
 from musician.models import Musician, MusicianForm
+from tagging.views import tagged_object_list
 
 info_dict = {
     'queryset': Musician.objects.all(),
@@ -14,6 +15,9 @@ urlpatterns = patterns('',
                         dict(form_class=MusicianForm, post_save_redirect="/musician/") ),
                        (r'^(?P<slug>[^/]+)/delete/?$', 'django.views.generic.create_update.delete_object',
                         dict(model=Musician, post_delete_redirect="/musician/") ),
+                       url(r'^tag/(?P<tag>[^/]+)/$',tagged_object_list,dict(queryset_or_model=Musician, paginate_by=100, allow_empty=True,
+                                                                            template_name="musician/musician_tag_list.html"),
+                           name='musician_tag_detail'),
                        (r'^(?P<slug>[^/]+)/edit_links/?$', 'musician.views.edit_links'),
                        (r'^(?P<slug>[^/]+)/$', 'django.views.generic.list_detail.object_detail', info_dict),
 )
